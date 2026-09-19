9b414183453d2fc53bc6e8e88b92ff78ed017cdf
Pulkit seth <2432427pulkit@gmail.com>
2026-09-19 22:51:55 +0530

    Time: 0 ms (100%), Space: 12.7 MB (16.41%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.cpp
+++ 0162-find-peak-element/0162-find-peak-element.cpp
@@ -8,11 +8,11 @@ public:
         }
         if(nums[0] == *max_element(nums.begin(), nums.end())) return 0;
         if(n == 1) return 0;
-        if(n == 3) {
-            if(nums[0] > nums[1] && nums[0] > nums[2]) return 0;
-            if(nums[1] > nums[0] && nums[1] > nums[2]) return 1;
-            if(nums[2] > nums[0] && nums[2] > nums[1]) return 2;
-        }
+        // if(n == 3) {
+        //     if(nums[0] > nums[1] && nums[0] > nums[2]) return 0;
+        //     if(nums[1] > nums[0] && nums[1] > nums[2]) return 1;
+        //     if(nums[2] > nums[0] && nums[2] > nums[1]) return 2;
+        // }
         // int ans = 0;
         for(int i = 1; i<n-1; i++){
             if(i  ==   n-1){
